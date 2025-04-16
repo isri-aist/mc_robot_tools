bdb983aa9f07af921e8ee1e25d1f68d6d94ee3c4
ThomasDuvinage <toto.duvinage@gmail.com>
2025-04-16 16:14:21 +0900

    feat(cmake): move add_yaml_module to RobotModule.cmake

--- cmake/RobotModule.cmake
+++ cmake/RobotModule.cmake
@@ -14,3 +14,12 @@ macro(generate_urdf_description NAME)
   install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/convex/${NAME} DESTINATION "${DATA_DIR}/convex" FILES_MATCHING PATTERN "*.txt")
   install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/meshes/${NAME} DESTINATION "${DATA_DIR}/meshes" FILES_MATCHING PATTERN "*.stl" PATTERN "*.STL" PATTERN "*.dae" PATTERN "*.DAE")
 endmacro(generate_urdf_description)
+
+
+macro(add_yaml_module NAME)
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/yaml/${NAME}.in.yaml "${CMAKE_CURRENT_BINARY_DIR}/yaml/${NAME}.yaml")
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/alias/${NAME}.in.yaml "${CMAKE_CURRENT_BINARY_DIR}/alias/${NAME}.yaml")
+  # Install the alias and yaml
+  install(FILES "${CMAKE_CURRENT_BINARY_DIR}/alias/${NAME}.yaml" DESTINATION "${MC_ROBOTS_ALIASES_DIRECTORY}")
+  install(FILES "${CMAKE_CURRENT_BINARY_DIR}/yaml/${NAME}.yaml" DESTINATION "${DATA_DIR}/yaml")
+endmacro()
