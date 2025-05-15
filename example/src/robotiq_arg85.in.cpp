7d8e964492cf581c5876a7a3c3f0ad3c0a79f930
ThomasDuvinage <toto.duvinage@gmail.com>
2025-05-15 16:41:04 +0900

    Add example

---
+++ example/src/robotiq_arg85.in.cpp
@@ -0,0 +1,39 @@
+#include "robotiq_arg85.h"
+
+#include <RBDyn/parsers/urdf.h>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+namespace
+{
+
+// This is set by CMake, see CMakeLists.txt
+static const std::string ROBOTIQ_ARG85_DESCRIPTION_PATH = "@DATA_DIR@";
+
+} // namespace
+
+namespace mc_robots
+{
+
+RobotiqArg85RobotModule::RobotiqArg85RobotModule() : mc_rbdyn::RobotModule(ROBOTIQ_ARG85_DESCRIPTION_PATH, "robotiq_arg85")
+{
+  // True if the robot has a fixed base, false otherwise
+  bool fixed = true;
+  // Makes all the basic initialization that can be done from an URDF file
+  init(rbd::parsers::from_urdf_file(urdf_path, fixed));
+
+  // Automatically load the convex hulls associated to each body
+  std::string convexPath = description_paths::convex_DIR;
+  fs::path p(convexPath);
+  if(fs::exists(p) && fs::is_directory(p))
+  {
+    std::vector<fs::path> files;
+    std::copy(fs::directory_iterator(p), fs::directory_iterator(), std::back_inserter(files));
+
+    // _convexHull["camera_interface_link"] = std::pair<std::string, std::string>("camera_interface_link", files[0].string());
+
+  }
+}
+
+} // namespace mc_robots
