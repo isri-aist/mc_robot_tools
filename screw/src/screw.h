734a4c11b7ed5a138bd94dff99b1dffb80dbba84
Khang Pham <phamthekhang632@gmail.com>
2026-06-05 04:10:08 +0200

    feat: support screw module

---
+++ screw/src/screw.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <mc_rbdyn/RobotModule.h>
+#include <mc_rbdyn/RobotModuleMacros.h>
+#include <mc_robots/api.h>
+#include <mc_rtc/logging.h>
+
+namespace mc_robots
+{
+
+struct MC_ROBOTS_DLLAPI ScrewRobotModule : public mc_rbdyn::RobotModule
+{
+  ScrewRobotModule(const std::string & name);
+};
+
+} // namespace mc_robots
+
+extern "C"
+{
+  ROBOT_MODULE_API void MC_RTC_ROBOT_MODULE(std::vector<std::string> & names) // NOLINT(readability-identifier-naming)
+  {
+    names = {"Screw"};
+  }
+
+  ROBOT_MODULE_API void destroy(mc_rbdyn::RobotModule * ptr)
+  {
+    delete ptr; // NOLINT(cppcoreguidelines-owning-memory)
+  }
+
+  ROBOT_MODULE_API mc_rbdyn::RobotModule * create(const std::string & n)
+  {
+    ROBOT_MODULE_CHECK_VERSION("Screw")
+
+    if(n == "Screw")
+    {
+      return new mc_robots::ScrewRobotModule("screw");
+    }
+
+    mc_rtc::log::error("Screw module cannot create an object of type {}", n);
+    return nullptr;
+  }
+}
