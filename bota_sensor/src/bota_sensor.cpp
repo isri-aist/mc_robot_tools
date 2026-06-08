eb67d366190f54569b0e9ef2b36f0bb94197a559
Khang Pham <phamthekhang632@gmail.com>
2026-06-08 09:36:50 +0200

    feat: add force and body sensor

--- bota_sensor/src/bota_sensor.cpp
+++ bota_sensor/src/bota_sensor.cpp
@@ -14,6 +14,10 @@ BotaSensorRobotModule::BotaSensorRobotModule(const std::string & name) : mc_rbdy
   bool fixed = false;
   init(rbd::parsers::from_urdf_file(urdf_path, fixed));
   rsdf_dir = (bfs::path(MC_RSDF_DIR) / name).string();
+
+  _forceSensors.emplace_back() =
+      mc_rbdyn::ForceSensor("BotaForceSensor", name + "_wrench", sva::PTransformd::Identity());
+  _bodySensors.emplace_back() = mc_rbdyn::BodySensor("BotaAccelerometer", name + "_imu", sva::PTransformd::Identity());
 }
 
 } // namespace mc_robots
